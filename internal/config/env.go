79a1f52708aaab699cdc6a5bda8190703195ef04
Wildan Nursyamsi <muhamad11wildan4n@gmail.com>
2026-05-28 17:22:04 +0700

    fix: update env load config

--- internal/config/env.go
+++ internal/config/env.go
@@ -8,15 +8,17 @@ import (
 )
 
 type Config struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBSSLMode  string
-	JWTSecret  string
+	DBHost       string
+	DBPort       string
+	DBUser       string
+	DBPassword   string
+	DBName       string
+	DBSSLMode    string
+	JWTSecret    string
+	AllowOrigins string
 
 	// Cloudinary
+
 	CloudinaryCloudName string
 	CloudinaryAPIKey    string
 	CloudinaryAPISecret string
@@ -28,15 +30,17 @@ func LoadConfig() *Config {
 	}
 
 	return &Config{
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", ""),
-		DBName:     getEnv("DB_NAME", "management_siswa"),
-		DBSSLMode:  getEnv("DB_SSL_MODE", "require"),
-		JWTSecret:  getEnv("JWT_SECRET", "supersecretkey"),
+		DBHost:       getEnv("DB_HOST", "localhost"),
+		DBPort:       getEnv("DB_PORT", "5432"),
+		DBUser:       getEnv("DB_USER", "postgres"),
+		DBPassword:   getEnv("DB_PASSWORD", ""),
+		DBName:       getEnv("DB_NAME", "management_siswa"),
+		DBSSLMode:    getEnv("DB_SSL_MODE", "require"),
+		JWTSecret:    getEnv("JWT_SECRET", "supersecretkey"),
+		AllowOrigins: getEnv("ALLOW_ORIGINS", "http://localhost:5173"),
 
 		CloudinaryCloudName: getEnv("CLOUDINARY_CLOUD_NAME", ""),
+
 		CloudinaryAPIKey:    getEnv("CLOUDINARY_API_KEY", ""),
 		CloudinaryAPISecret: getEnv("CLOUDINARY_API_SECRET", ""),
 	}
